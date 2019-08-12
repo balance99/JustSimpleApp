9128db91234288077d98a5e601dd4c5f759575cd
retheviper <youngbina@hotmail.com>
2019-08-12 20:10:03 +0900

    주석 추가

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -4,12 +4,13 @@ import { GiftedChat } from 'react-native-gifted-chat';
 import SQLite from 'react-native-sqlite-storage';
 
 // 서버 주소
+// insert는 메시지 송신 시 DB에 기록
 const API = 'http://3.112.67.233:3002/chat/insert';
+// select는 화면 기동 시 과거 대화 내용 불러옴
 const API2 = 'http://3.112.67.233:3002/chat/select';
 
 // 메인 채팅 화면
 export default class chatScreen extends React.Component {
-
 
   constructor(props) {
     super(props);
@@ -20,16 +21,16 @@ export default class chatScreen extends React.Component {
     };
   }
 
-// 예시 메시지
+// 메시지 세팅
   componentWillMount() {
     const { navigation } = this.props;
     const id = navigation.getParam("id");
     this.getPreviousMessages(id);
-
   }
 
   // 이전 대화내용 불러오기
   // TODO 어디까지 불러올 것인가 결정(DB SELECT 단계부터 for문 표시까지)
+  // TODO 대화 구분 (내 메시지인가, 상대방인가)
   getPreviousMessages(id){
     fetch(API2, {
       method: 'POST',
@@ -95,12 +96,13 @@ export default class chatScreen extends React.Component {
   }
 
 // 로그인 화면으로 이동
-// TODO 로그아웃 기능 추가
+// TODO 로그인/로그아웃 기능 추가
   moveBack() {
     const { navigate } = this.props.navigation;
     navigate('Home')
   }
 
+// 현재 more버튼 누를 시 로그인 화면으로 돌아감
   render() {
     return (
     <Container>
